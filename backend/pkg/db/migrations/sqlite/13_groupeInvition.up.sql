7bbe291799ef9c6e773be5b6afc840c6f0e9d492
mouhametadiouf <diegargonefaye@gmail.com>
2024-02-01 18:22:26 +0000

    add some tables

---
+++ backend/pkg/db/migrations/sqlite/13_groupeInvition.up.sql
@@ -0,0 +1,12 @@
+-- CREATE TABLE IF NOT EXISTS group_invitations (
+--     id VARCHAR PRIMARY KEY,
+--     user_id VARCHAR,
+--     group_id VARCHAR,
+--     sender_id VARCHAR,
+--     status VARCHAR NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     updated_at TIMESTAMP,
+--     FOREIGN KEY (user_id) REFERENCES users(id),
+--     FOREIGN KEY (group_id) REFERENCES groups(id),
+--     FOREIGN KEY (sender_id) REFERENCES users(id)
+-- 	);
