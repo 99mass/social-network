8629f3fe926685bf6869bae45b6cfa28c084b4dc
osamb <omarsamb955@gmail.com>
2024-02-02 15:45:15 +0000

    update

--- backend/pkg/handler/loginHandler.go
+++ backend/pkg/handler/loginHandler.go
@@ -4,17 +4,17 @@ import (
 	"database/sql"
 	"encoding/json"
 	"log"
+	"main/pkg/controller"
 	"main/pkg/helper"
 	"main/pkg/models"
 	"main/pkg/utils"
 	"net/http"
 )
 
-type loginReq struct{
-	Email string `json:"email"`
+type loginReq struct {
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
-
 
 func LoginHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -23,18 +23,43 @@ func LoginHandler(db *sql.DB) http.HandlerFunc {
 			var logReq loginReq
 			err := json.NewDecoder(r.Body).Decode(&logReq)
 			if err != nil {
-				helper.SendResponse(w,models.ErrorResponse{
-					Status: "error",
+				helper.SendResponse(w, models.ErrorResponse{
+					Status:  "error",
 					Message: "incorrect request",
-				},http.StatusBadRequest)
+				}, http.StatusBadRequest)
 				return
 			}
-			okLogin,userID,err := utils.CheckLogin(logReq.Email,logReq.Password,db)
+			okLogin, userID, err := utils.CheckLogin(logReq.Email, logReq.Password, db)
 
-			log.Println("is it ok : ",okLogin)
-			log.Println("the UserID: ",userID)
-			log.Println("the error : ",err)
+			if !okLogin {
+				helper.SendResponse(w, models.ErrorResponse{
+					Status:  "error",
+					Message: err.Error(),
+				}, http.StatusBadRequest)
+				return
+			}
 
+			sessionID, err := helper.AddSession(userID, db)
+			if err != nil {
+				helper.SendResponse(w, models.ErrorResponse{
+					Status:  "error",
+					Message: "incorrect request",
+				}, http.StatusBadRequest)
+				return
+			}
+			session, err := controller.GetSessionByID(db, sessionID)
+			if err != nil {
+				helper.SendResponse(w, models.ErrorResponse{
+					Status:  "error",
+					Message: "we got an issue",
+				}, http.StatusInternalServerError)
+				return
+			}
+			log.Println("succesfully login")
+			helper.SendResponse(w, models.SessionToSend{
+				Value:      session.ID,
+				Expiration: session.ExpiresAt,
+			}, http.StatusOK)
 
 		default:
 			helper.SendResponse(w, models.ErrorResponse{
