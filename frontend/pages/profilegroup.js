9319d172fb11225321d414f623835f434aa9d4b4
ssambadi <you@example.com>
2024-02-08 17:07:07 +0000

    logout

--- frontend/pages/profilegroup.js
+++ frontend/pages/profilegroup.js
@@ -1,17 +1,17 @@
 import Head from "next/head";
-import Sign_in from "../components/auth/signin_page";
+import Header from "../components/header";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { getSessionCookie, isValideSession } from "../utils/cookies";
+import { isValideSession } from "../utils/cookies";
+import Profile_group from "../components/profilegroup/profile_group";
 
 export default function SignIn() {
   const router = useRouter();
-  
   useEffect(() => {
     const checkSession = async () => {
       const isValid = await isValideSession();
-      if (isValid) {
-        router.push("/home");
+      if (!isValid) {
+        router.push("/");
       }
     };
 
@@ -21,11 +21,14 @@ export default function SignIn() {
   return (
     <>
       <Head>
-        <title>Social-network/Authentification</title>
+        <title>Social-network/profile-group</title>
         <meta name="description" content="project social-network" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Sign_in />
+      <Header />
+      <section className="section-group-profile">
+        <Profile_group/>
+      </section>
     </>
   );
 }
