c7274304bae8db15ce6d7e0ec45b7f2ac2734241
osamb <omarsamb955@gmail.com>
2024-02-09 11:30:36 +0000

    adding update profil handler for updating the user informations

---
+++ backend/pkg/handler/updateProfil.go
@@ -0,0 +1,77 @@
+package handler
+
+import (
+	"backend/pkg/controller"
+	"backend/pkg/helper"
+	"backend/pkg/models"
+	"backend/pkg/utils"
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gofrs/uuid"
+)
+
+func UpdateProfil(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			session := r.Header.Get("SessionID")
+			sessId, err := uuid.FromString(session)
+			if err != nil {
+				helper.SendResponse(w, models.ErrorResponse{
+					Status:  "error",
+					Message: "format value session incorrect",
+				}, http.StatusBadRequest)
+				return
+			}
+			sess, err := controller.GetSessionByID(db, sessId)
+			if err != nil {
+				helper.SendResponse(w, models.ErrorResponse{
+					Status:  "error",
+					Message: "you're not authorized",
+				}, http.StatusBadRequest)
+				return
+			}
+			var user models.User
+			err = json.NewDecoder(r.Body).Decode(&user)
+			if err != nil {
+				helper.SendResponse(w, models.ErrorResponse{
+					Status:  "error",
+					Message: "incorrect request",
+				}, http.StatusBadRequest)
+				return
+			}
+			checkRegister, err := utils.CheckRegisterFormat(user.FirstName, user.LastName,
+				 user.Nickname, user.Email, user.Password, user.Password, user.DateOfBirth, db)
+
+			if !checkRegister {
+				helper.SendResponse(w, models.ErrorResponse{
+					Status:  "error",
+					Message: err.Error(),
+				}, http.StatusBadRequest)
+				return
+			}
+
+			user.ID = sess.UserID.String()
+			//log.Println("info user to update", user)
+			err = controller.UpdateUser(db, user)
+			if err != nil {
+				helper.SendResponse(w, models.ErrorResponse{
+					Status:  "error",
+					Message: "we got an issue",
+				}, http.StatusInternalServerError)
+				return
+			}
+			helper.SendResponse(w, user , http.StatusOK)
+
+		default:
+			helper.SendResponse(w, models.ErrorResponse{
+				Status:  "error",
+				Message: "Method not allowed",
+			}, http.StatusMethodNotAllowed)
+			log.Println("methods not allowed")
+		}
+	}
+}
