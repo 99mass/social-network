8b9c1392bc4f62371a4828249f4da2ed0e7f43ce
osamb <omarsamb955@gmail.com>
2024-02-12 13:17:59 +0000

    completed the addpost module

--- backend/pkg/models/post.go
+++ backend/pkg/models/post.go
@@ -1,11 +1,11 @@
 package models
 
-type Post struct{
-	ID string `db:"id" json:"id"`
-	UserID string `db:"user_id" json:"user_id"`
-	GroupID string `db:"group_id" json:"group_id"`
-	Content string `db:"content" json:"content"`
+type Post struct {
+	ID        string `db:"id" json:"id"`
+	UserID    string `db:"user_id" json:"user_id"`
+	GroupID   string `db:"group_id" json:"group_id"`
+	Content   string `db:"content" json:"content"`
 	ImagePath string `db:"image_path" json:"image_path"`
-	Privacy string `db:"privacy" json:"privacy"`
+	Privacy   string `db:"privacy" json:"privacy"`
 	CreatedAt string `db:"created_at" json:"created_at"`
 }
