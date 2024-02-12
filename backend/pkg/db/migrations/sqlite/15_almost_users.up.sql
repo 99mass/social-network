6ed15191928708780835c608c09cb367a582d9c2
osamb <omarsamb955@gmail.com>
2024-02-12 18:05:01 +0000

    update almost user, handle table almost users

--- backend/pkg/db/migrations/sqlite/15_almost_users.up.sql
+++ backend/pkg/db/migrations/sqlite/15_almost_users.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS almost_users (
     post_id VARCHAR,
     user_id VARCHAR,
-    authorize_users []VARCHAR
+    authorize_users VARCHAR
     PRIMARY KEY (post_id, user_id),
     FOREIGN KEY (post_id) REFERENCES posts(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
