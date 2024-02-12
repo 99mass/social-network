52d3aef8091c92bc20413727adc8f5fbf1968b09
mouhametadiouf <diegargonefaye@gmail.com>
2024-02-12 14:55:23 +0000

    Add connected user

--- backend/pkg/routes/route.go
+++ backend/pkg/routes/route.go
@@ -14,6 +14,7 @@ func Route(db *sql.DB) {
 	http.HandleFunc("/session",helper.Middleware(handler.CheckSessionHandler(db)))
 	http.HandleFunc("/logout",helper.Middleware(handler.LogOutHandler(db)))
 	http.HandleFunc("/profil",helper.Middleware(handler.ProfilHandler(db)))
+	http.HandleFunc("/user",helper.Middleware(handler.ConnectedUser(db)))
 	http.HandleFunc("/update_profil",helper.Middleware(handler.UpdateProfil(db)))
 	http.HandleFunc("/addpost", helper.Middleware(handler.PostHandler(db)))
 	http.HandleFunc("/followuser", helper.Middleware(handler.FollowUser(db)))
