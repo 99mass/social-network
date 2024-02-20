20cc8cf32b8119a59838589b1c3e2c9780e2022c
bayerane <bayerane89@hotmail.fr>
2024-02-20 15:50:13 +0000

    model and controller private messages

---
+++ backend/pkg/models/private_messages.go
@@ -0,0 +1,10 @@
+package models
+
+// PrivateMessage represente un message privé entre utilisateurs
+type PrivateMessages struct {
+	ID          string `db:"id" json:"id"`
+	SenderID    string `db:"sender_id" json:"sender_id"`
+	RecipientID string `db:"recipient_id" json:"recipient_id"`
+	Content     string `db:"content" json:"content"`
+	CreatedAt   string `db:"created_at" json:"created_at"`
+}
