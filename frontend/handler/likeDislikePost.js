35138ddc443a8fb492211a72c9f9c97e05983a40
ssambadi <you@example.com>
2024-02-21 12:58:04 +0000

    update

--- frontend/handler/likeDislikePost.js
+++ frontend/handler/likeDislikePost.js
@@ -1,32 +1,43 @@
 import { api } from "../utils/api";
 import { getSessionCookie } from "../utils/cookies";
-import { getPostsUser, getPostsUserCreated } from "./getPostsUser";
+import {
+  getPostsUser,
+  getPostsUserCreated,
+  getSpecificPostsUser,
+} from "./getPostsUser";
 
-export const likeDislikePost = async (userid,postid,is_liked, setPosts,setPostsCreated) => {
-   
-    const data = {
-        post_id: postid,
-        action: is_liked ? "dislike" : "like",
-      }
-    try {
-        const sessionId = getSessionCookie();
+export const likeDislikePost = async (
+  userid,
+  postid,
+  is_liked,
+  setPosts,
+  setPostsCreated,
+  setSpecificPostData
+) => {
+  const data = {
+    post_id: postid,
+    action: is_liked ? "dislike" : "like",
+  };
+  try {
+    const sessionId = getSessionCookie();
 
-        const response = await fetch(api.LikeDslikepost, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: sessionId,
-            },
-            body: JSON.stringify(data),
-        });
+    const response = await fetch(api.LikeDslikepost, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: sessionId,
+      },
+      body: JSON.stringify(data),
+    });
 
-        // Vérifier le statut de la réponse
-        if (response.ok) {
-          if(setPosts)  getPostsUser(setPosts);
-          if(setPostsCreated)  getPostsUserCreated(userid, setPostsCreated);
-        }
-
-    } catch (error) {
-        console.error("Error fetching profile data:", error.message);
+    // Vérifier le statut de la réponse
+    if (response.ok) {
+      if (setPosts) getPostsUser(setPosts);
+      if (setPostsCreated) getPostsUserCreated(userid, setPostsCreated);
+      if (setSpecificPostData)
+        getSpecificPostsUser(postid, setSpecificPostData);
     }
+  } catch (error) {
+    console.error("Error fetching profile data:", error.message);
+  }
 };
