2712311937c77437cd6da7bc8415e9d6cc6d9040
bayerane <bayerane89@hotmail.fr>
2024-02-21 15:26:10 +0000

    update

--- backend/pkg/db/migrations/sqlite/7_groupEvents.up.sql
+++ backend/pkg/db/migrations/sqlite/7_groupEvents.up.sql
@@ -4,7 +4,5 @@ CREATE TABLE IF NOT EXISTS group_events (
     title VARCHAR NOT NULL,
     description TEXT,
     day_time DATETIME NOT NULL,
-    option1_count INTEGER DEFAULT 0,
-    option2_count INTEGER DEFAULT 0,
     FOREIGN KEY (group_id) REFERENCES groups(id)
 );
