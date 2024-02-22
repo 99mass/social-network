db733bd9a78073f02627aa59f92426e5dfa12f0f
osamb <omarsamb955@gmail.com>
2024-02-22 11:22:33 +0000

    private message through websocket done V

---
+++ backend/pkg/webSocket/privateChat.go
@@ -0,0 +1,137 @@
+package websocket
+
+import (
+	"backend/pkg/controller"
+	"backend/pkg/helper"
+	"backend/pkg/models"
+	"backend/pkg/utils"
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type GenResponse struct {
+	Type    string      `json:"type"`
+	Content interface{} `json:"content"`
+}
+
+var users map[string]*models.UserConnected = make(map[string]*models.UserConnected)
+
+// var userList []models.UserConnected
+
+func ChatHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		sess, err := utils.CheckAuthorization(db, w, r)
+		if err != nil {
+			helper.SendResponseError(w, "error", "you're not authorized", http.StatusBadRequest)
+			log.Println("not auth")
+			return
+		}
+		upgrader := websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool { return true },
+		}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if user, ok := users[sess.UserID.String()]; ok {
+			// Si l'utilisateur existe déjà, mettez à jour la connexion
+			user.Conn = conn
+		} else {
+			// Sinon, créez un nouvel utilisateur
+			users[sess.UserID.String()] = &models.UserConnected{Conn: conn, UserID: sess.UserID.String()}
+		}
+		log.Println("list of connected users: ", users)
+		go HandleMessages(db, conn, sess.UserID.String())
+	}
+}
+
+func HandleMessages(db *sql.DB, conn *websocket.Conn, userID string) {
+	for {
+
+		var message models.PrivateMessages
+		err := conn.ReadJSON(&message)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("this is the message to sent:", message)
+		if err == nil {
+			if len(message.Content) > 100 {
+				SendGenResponse("error", conn, "message is too long")
+				continue
+			}
+			if message.SenderID == "" || message.RecipientID == "" || message.Content == "" {
+				SendGenResponse("error", conn, "invalid type for message")
+				continue
+			}
+			err = SendMessage(db, message)
+			if err != nil {
+				SendGenResponse("error", conn, err.Error())
+				continue
+			}
+		}
+
+	}
+}
+
+func SendMessage(db *sql.DB, message models.PrivateMessages) error {
+	ok, err := controller.AreUsersFriends(db, message.SenderID, message.RecipientID)
+	if err != nil {
+		log.Println("users are not friend:", err.Error())
+		return errors.New("users are not friend")
+	}
+
+	if !ok {
+		log.Println("users are not friend:")
+		return errors.New("you must be friend first")
+	}
+	message.CreatedAt = time.Now().Format("2006-01-02 15:04:05")
+	if user, ok := users[message.RecipientID]; ok {
+		err := SendGenResponse("message", user.Conn, message)
+		if err != nil {
+			log.Println("Private Message was not sent")
+			return errors.New("enable to send your message")
+		}
+	}
+	err = SaveMessage(db, message.SenderID, message.RecipientID, message.Content)
+	if err != nil {
+		log.Println(err.Error())
+		return errors.New("enable to save your message")
+	}
+
+	log.Println("message sent successfully to : " + message.RecipientID)
+	return nil
+}
+
+func SaveMessage(db *sql.DB, sender string, recipient string, message string) error {
+	var Mes models.PrivateMessages
+	Mes.SenderID = sender
+	Mes.RecipientID = recipient
+	Mes.Content = message
+	_, err := controller.CreateMessage(db, Mes)
+	if err != nil {
+		return errors.New("can't create message : " + err.Error())
+	}
+	log.Println("message saved successfully")
+	return nil
+}
+
+func SendGenResponse(name string, conn *websocket.Conn, message interface{}) error {
+	var res GenResponse
+	res.Type = name
+	res.Content = message
+	err := conn.WriteJSON(res)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
