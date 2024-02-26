6511d579ddd9e647834d9724d305c7ad732c2b3f
alogou <alaingildasogou@gmail.com>
2024-02-26 16:01:41 +0000

    update

--- frontend/components/profilegroup/discussions.js
+++ frontend/components/profilegroup/discussions.js
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getPostsGroup } from "../../handler/getPostsGroup";
 import { getElapsedTime } from "../../utils/convert_dates";
 import styles from "../../styles/modules/profile-group.module.css";
 import { PostFooter, PostHeader, PostMiddle } from "../home/middle_bloc";
+import { useRouter } from "next/router";
 
 export default function Discussion({ description}) {
   const router = useRouter();
@@ -11,15 +12,10 @@ export default function Discussion({ description}) {
   // const [comment, setComment] = useState(null);
   // let id = "546f7041-7185-41d6-8634-d5e97d3825c7"
   // console.log("grosupid:", id);
-export default function Discussion({
-  postGroup,
-  setPostsGroup,
-  groupId,
-  description,
-}) {
   useEffect(() => {
-    if (groupId) {
-      getPostsGroup(groupId, setPostsGroup);
+    if (id) {
+      getPostsGroup(id, setPostsGroup);
+      // console.log("postgroup data:",postData)
     }
   }, []);
   // iduser,
@@ -43,7 +39,7 @@ export default function Discussion({
                 time={`${getElapsedTime(item.post.created_at).value} ${
                   getElapsedTime(item.post.created_at).unit
                 }`}
-                groupid={groupId}
+                groupid={id}
                 setPostsGroup={setPostsGroup}
               />
               <PostMiddle
@@ -56,7 +52,7 @@ export default function Discussion({
                 numberComment={item.nbr_comments}
                 userid={item.user.id}
                 postid={item.post.id}
-                groupid={groupId}
+                groupid={id}
                 setPostsGroup={setPostsGroup}
               />
             </div>
