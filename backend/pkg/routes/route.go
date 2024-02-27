c49b85f6ea0bca548d82f79060e0c9ce192a35a4
osamb <omarsamb955@gmail.com>
2024-02-27 11:00:49 +0000

    updater

--- backend/pkg/routes/route.go
+++ backend/pkg/routes/route.go
@@ -14,6 +14,7 @@ func Route(db *sql.DB) {
 	//WebSockets
 	http.HandleFunc("/global_socket", websocket.ChatHandler(db))
 	http.HandleFunc("/discussion", websocket.CommunicationHandler(db))
+	http.HandleFunc("/group_chat", websocket.PrivateGroupChat(db))
 
 	//User Actions
 	http.HandleFunc("/register", helper.Middleware(handler.RegisterHandler(db)))
