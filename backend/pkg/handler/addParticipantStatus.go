41a54736fff6f9cc289b218e72df5def79724a14
bayerane <bayerane89@hotmail.fr>
2024-02-29 19:12:01 +0000

    create participants

--- backend/pkg/handler/addParticipantStatus.go
+++ backend/pkg/handler/addParticipantStatus.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"database/sql"
 	"errors"
+	"log"
 	"net/http"
 
 	"backend/pkg/controller"
@@ -14,7 +15,7 @@ import (
 func AddEventParticipantHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
-		case http.MethodPost:
+		case http.MethodGet:
 			// Assurez-vous que l'utilisateur est authentifié et récupérez son ID
 			sess, err := utils.CheckAuthorization(db, w, r)
 			if err != nil {
@@ -25,10 +26,12 @@ func AddEventParticipantHandler(db *sql.DB) http.HandlerFunc {
 			// Extraire l'ID de l'événement et le statut de participation de la requête
 			eventID := r.URL.Query().Get("event_id")
 			participantStatus := r.URL.Query().Get("choosen_option")
-
+			if eventID == "" {
+				return
+			}
 			// Convertir le statut de participation en int (1 pour Going,   0 pour Not Going)
 			var status int
-			if participantStatus == "Going" {
+			if participantStatus == "1" {
 				status = 1
 			} else {
 				status = 0
@@ -37,14 +40,16 @@ func AddEventParticipantHandler(db *sql.DB) http.HandlerFunc {
 			// Vérifier si l'utilisateur a déjà un statut de participation pour cet événement
 			existingStatus, err := controller.GetParticipantStatus(db, eventID, sess.UserID.String())
 			if err != nil && !errors.Is(err, sql.ErrNoRows) {
+				log.Printf("Error checking participation status: %v\n", err)
 				helper.SendResponseError(w, "error", "Failed to check participation status", http.StatusInternalServerError)
 				return
 			}
 
 			// Si l'utilisateur a déjà un statut de participation, mettre à jour
-			if existingStatus != 0 {
+			if existingStatus != 2 {
 				err = controller.UpdateParticipantStatus(db, eventID, sess.UserID.String(), status)
 				if err != nil {
+					log.Printf("Error updating participation status: %v\n", err)
 					helper.SendResponseError(w, "error", "Failed to update participation status", http.StatusInternalServerError)
 					return
 				}
@@ -57,6 +62,7 @@ func AddEventParticipantHandler(db *sql.DB) http.HandlerFunc {
 				}
 				err = controller.CreateParticipantStatus(db, participant)
 				if err != nil {
+					log.Printf("Error creating participation: %v\n", err)
 					helper.SendResponseError(w, "error", "Failed to create participation", http.StatusInternalServerError)
 					return
 				}
