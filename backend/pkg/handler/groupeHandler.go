6507795a242c4a8041f24c4f257c96f5f6f473f3
osamb <omarsamb955@gmail.com>
2024-02-29 11:42:35 +0000

    handling the error about the function SendGenResponse who is used in two websockets

--- backend/pkg/handler/groupeHandler.go
+++ backend/pkg/handler/groupeHandler.go
@@ -8,6 +8,7 @@ import (
 	"backend/pkg/controller"
 	"backend/pkg/helper"
 	"backend/pkg/utils"
+	websocket "backend/pkg/webSocket"
 )
 
 func GetMyGroupsHandler(db *sql.DB) http.HandlerFunc {
@@ -73,6 +74,7 @@ func GroupsIManageHandler(db *sql.DB) http.HandlerFunc {
 			}
 
 			helper.SendResponse(w, groups, http.StatusOK)
+			websocket.BroadcastUserList(db)
 		default:
 			helper.SendResponseError(w, "error", "method not allowed", http.StatusMethodNotAllowed)
 			log.Println("methods not allowed")
