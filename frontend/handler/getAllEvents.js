58ecd566a767e25b13bffed4aaededfd1bc84111
alo <ass.lo1998@gmail.com>
2024-02-29 11:45:39 +0000

    UpdateParticipants

--- frontend/handler/getAllEvents.js
+++ frontend/handler/getAllEvents.js
@@ -28,3 +28,32 @@ export const ListAllEvents = async (groupId, setAllEvents) => {
         }
     }
 };
+
+
+export const eventPartipants = async (eventId, chosenOption, setDatas) => {
+    if (groupId) {
+        try {
+            const sessionId = getSessionCookie();
+
+            const response = await fetch(`${api.Create_participants_event}?event_id=${eventId}?chosen_option=${chosenOption}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: sessionId,
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data ,"event");
+                setDatas(data);
+            }
+            else{
+                const data = await response.json();
+                console.log('error', data.message);
+            }
+        } catch (error) {
+            console.error("Error fetching profile data:", error.message);
+        }
+    }
+};
